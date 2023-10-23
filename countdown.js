65e88c2005da717cdf92342e5b0e7c0dbe6ce1e3
Ronit Banerjee <103939129+Ronit-Banerjee-78@users.noreply.github.com>
2023-10-23 10:40:53 +0530

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -3,10 +3,15 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 
+const day = document.getElementById("day");
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
 
 
-const newTime = new Date('Oct 19, 2023 00:00:00').getTime();
 
+const newTime = new Date('Oct 8, 2024 00:00:00').getTime();
+const maaKali= new Date('Nov 11, 2023 00:00:00').getTime();
 
 function upadteTime() {
     const currentTime = new Date().getTime();
@@ -18,6 +23,8 @@ function upadteTime() {
     const m = Math.floor(durga / 1000 / 60) % 60;
     const s = Math.floor(durga / 1000) % 60;
 
+
+
     // console.log(d);
     // console.log(h);
     // console.log(m);
@@ -28,6 +35,25 @@ function upadteTime() {
     minutes.innerHTML = m < 10 ? "0" + m : m;
     seconds.innerHTML = s < 10 ? "0" + s : s;
 
+
+
+}
+function updatetime(){
+    const currentTime = new Date().getTime();
+    const kali=maaKali-currentTime
+
+
+
+    const kd = Math.floor(kali / (1000 * 60 * 60 * 24));
+    const kh = Math.floor(kali / 1000 / 60 / 60) % 24;
+    const km = Math.floor(kali / 1000 / 60) % 60;
+    const ks = Math.floor(kali / 1000) % 60;
+
+    day.innerHTML = kd;
+    hour.innerHTML = kh < 10 ? "0" + kh : kh;
+    minute.innerHTML = km < 10 ? "0" + km : km;
+    second.innerHTML = ks < 10 ? "0" + ks : ks;
 }
 
 setInterval(upadteTime, 1000);
+setInterval(updatetime, 1000);
